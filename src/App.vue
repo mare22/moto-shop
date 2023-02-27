582d5db1b04f79b6a37a601a396c6a9735119569
Mane <manegit1996>
2023-02-27 21:12:18 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,190 @@
+<template>
+    <!----------------------- TOP ------------------------------>
+    <div class="top">
+        <div class="header">
+            <div class="navbar">
+                <div class="wrapper">
+                        <a href="" alt=""><img class="logo" src="../src/assets/images/logo.png"></a>
+                        <nav>
+                            <input type="checkbox" id="check">
+                            <label for="check" class="hamburger-btn">
+                                <i class="fas fa-bars" style="color: white;"></i>
+                            </label>
+                            <ul class="nav-list">
+                                <li><a class="active" href="">HOME</a></li> 
+                                <li><a class="active"  href="">ABOUT US</a></li>
+                                <li><a class="active"  href="">NEWS</a></li>
+                                <li><a class="active"  href="">CONTACT</a></li>
+                                <li ><a href=""><i class="fa-solid fa-cart-shopping"></i></a></li>
+                            </ul>
+                        </nav>
+                </div> <!--end.wrapper-->
+            </div> <!--end.navbar-->
+        </div> <!--end.header-->
+
+
+        <div class="intro">
+            <div class="wrapper">
+                <h1>Welcome to Our website.One of the largest motorcycle stores in Europe.<br>
+                    Discover the best offers on our website...</h1>
+            </div> <!--end.wrapper-->
+        </div> <!--end.intro-->
+       
+    </div> <!--end.top-->
+
+
+    <!------------------ARTICLES------------------->
+    <div class="articles">
+            <div class="wrapper">
+            <h2>See our offer that we have prepared for You!</h2>
+            <p>Here you can find the best helmets, jackets, gloves and footwear for driving on the road in one place - both for longer trips (Touring) <br>and for cruising the city streets (Urban-City). We know that motorcycling is not only a hobby, but also a sport, so if you want to equip yourself for maximum adrenaline pleasure, you will find equipment and protectors intended for sports riding on the track.
+            </p>
+        </div> <!--end.wrapper-->
+
+        <div class="content-wrapper">
+            
+            <div class="content-box">
+                <div class="content-img">
+                    <a href="" alt=""><img src="../src/assets/content-images/helmetfinal.png" alt=""></a>
+                </div> <!--end.content-img-->
+                <h4>HELMETS</h4>
+            </div> <!--end.content-box-->
+
+            <div class="content-box">
+                <div class="content-img">
+                    <a href="" alt=""><img src="../src/assets/content-images/jacketfinal.png" alt=""></a>
+                </div>
+                <h4>JACKETS</h4>
+            </div> <!--end.content-box-->
+            <div class="content-box">
+                <div class="content-img">
+                    <a href="" alt=""><img class="gloves" src="../src/assets/content-images/gfinal.png" alt=""> </a>
+                </div>
+                <h4>GLOVES</h4>
+            </div> <!--end.content-box-->
+            <div class="content-box">
+                <div class="content-img">
+                    <a href="" alt=""><img src="../src/assets/content-images/shoesfinal.png" alt=""></a>
+                </div>
+                <h4>SHOES</h4>
+            </div> <!--end.content-box-->
+            <div class="content-box">
+                <div class="content-img">
+                    <a href="" alt=""> <img src="../src/assets/content-images/accfinal.png" alt=""></a>
+                </div>
+                <h4>ACCESSORIES</h4>
+            </div> <!--end.content-box-->
+        </div>
+    </div> <!--end.articles-->
+    <!-----------END ARTICLES----------------------->
+
+    <!---------BRANDS------------------>
+
+    <div class="brands">
+        <h1>Brands</h1>
+        <p>We have tried to always offer the latest and best models of Scorpion helmets, Ikon clothes, TCKS shoes, Puig tuning parts and 20 other brands,<br> all at popular prices, with complete service and fast delivery.
+        </p>
+        <div class="brands-wrapper">
+            <img src="../src/assets/brands/scorpiont.png" alt="">
+            <img src="../src/assets/brands/ixont.png" alt="">
+            <img src="../src/assets/brands/spykett.png" alt="">
+            <img src="../src/assets/brands/tcxtt.png" alt="">
+            <img src="../src/assets/brands/puigt.jpg.png" alt="">
+        </div><!--end.brands-wrapper-->
+        <div class="brands2-wrapper">
+            <img src="../src/assets/brands/lampat.png" alt="">
+            <img src="../src/assets/brands/furygant.png" alt="">
+            <img src="../src/assets/brands/galfert.png" alt="">
+            <img src="../src/assets/brands/kyttt.png" alt="">
+            <img src="../src/assets/brands/suomyt.png" alt="">
+        </div><!--end.brands2-wrapper-->
+    </div> <!--end.brands-->
+
+    <!-----------END BRANDS----------------->
+
+
+    <!------------------NEWS-------------------->
+    <!------------NEWS------------------->
+
+    <div class="news">
+        <h1>News</h1>
+        <div class="intro-text">
+            <p>Find out interesting news from the world of motorcycles,Moto GP races and novelties in our offer
+            </p>
+        </div><!--end.intro-text-->
+        <div class="news-wrapper">
+
+            <div class="article-outer">
+                    <div class="img-container">
+                       <a href=""><img src="../src/assets/news/fb.jpg" alt=""> </a>
+                    </div> <!--end.img-container-->
+                    <h2>
+                        The Sun Daily
+                        Pecco rakes up seventh season win with Malaysian GP victory
+                    </h2>
+                    <div class="entry-content">
+                        <p>Born in Turin, Bagnaia was already successful early on in Minimoto, as he won the European MiniGP championship in 2009...
+                        </p>
+                        <a href=""><button class="read-c">READ CONTENT</button></a> 
+                    </div><!--end.entry-content-->
+            </div><!--end.article-outer-->
+
+            <div class="article-outer">
+                <div class="img-container">
+                   <a href=""><img src="../src/assets/news/2.jpg" alt=""> </a>
+                </div> <!--end.img-container-->
+                <h2>
+                    Scorpion EXO-TECH new <br>Flip-Back
+                </h2>
+                <div class="entry-content">
+                    <p>It is the first Flip-Back helmet from the Scorpion brand. It is currently the most modern and... </p>
+                    <a href=""><button class="read-c">READ CONTENT</button></a> 
+                </div><!--end.entry-content-->
+        </div><!--end.article-outer-->
+
+        <div class="article-outer">
+            <div class="img-container">
+               <a href=""><img src="../src/assets/news/3.jpg" alt=""> </a>
+            </div> <!--end.img-container-->
+            <h2>Suomy and KYT models at purchase prices
+            </h2>
+            <div class="entry-content">
+                <p>SUOMY HALO (integral helmet with sun visor and free Pinlock) is now at the lowest price of 15,000 dinars for one color and 18,000...
+                </p>
+               <a href="index.html"><button class="read-c">READ CONTENT</button></a> 
+            </div><!--end.entry-content-->
+        </div><!--end.article-outer-->
+
+        </div><!--end.news-wrapper-->
+    </div><!--end.news-->
+    <!-----------------END NEWS--------------------->
+
+    
+     <!-------TNX-CUSTOMERS-------->
+     <div class="tnx-customers">
+        <div class="bg-hero">
+                <div class="wrapper">
+                
+                    <p> <i>You can get anywhere you want with a motorcycle, and you always carry your love and passion for motorcycling in your heart. We on our site never forget that, so we transfer that passion to the business we do. We have been successfully operating on the market for years and we are recognizable as one of the leaders in the sale of motorcycle equipment, both in our store and throughout Europe through the Web Shop.
+                    </i></p>
+                    <h2>We thank all our customers for their long-term trust,<br>
+                    and for us, your safety is paramount.</h2>
+                    <a href=""><button class="view-offer">VIEW OUR OFFER</button></a>
+                </div><!--end.wrapper-->
+        </div><!--end.bg-hero-->
+              
+    </div><!--end.txt-customers-->
+  <!-- <RouterView /> -->
+</template>
+
+<script>
+// import { RouterView } from 'vue-router'
+
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+
+</style>
